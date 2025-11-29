8c57e988bf9da96ddb4a2eec0f1e011de7f3dbda
Franco Bottaro <francoabottaro@gmail.com>
2025-11-29 01:14:02 -0300

    state server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@HttpCode(HttpStatus.OK)
-	@Get()
+	@Get("health")
 	getServer() {
 		return this.appService.getServer();
 	}
